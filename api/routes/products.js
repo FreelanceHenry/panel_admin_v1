70a7377fc33b759a5004a37c6e23163c50b5f02e
Henry Carrillo <henry.toxx@gmail.com>
2024-05-01 16:09:20 -0400

    add new services and routes in product

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,11 +1,23 @@
-import express from 'express'
-import productService from '../services/product.js';
+import express from "express";
+import productService from "../services/product.js";
 
-const router = express.Router(); 
+const router = express.Router();
 
 router.get("/", async (req, res) => {
   try {
     res.json(await productService.getProducts());
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+//ACCESS ALL USERS
+//POST Return SingleProduct
+router.post("/", async (req, res) => {
+  const id = req.body.singleProductId;
+  try {
+    res.json(await productService.getSingleProduct(id));
   } catch (error) {
     console.error("Error executing query:", error);
     res.status(500).send("Internal Server Error");
--- api/services/product.js
+++ api/services/product.js
@@ -1,4 +1,4 @@
-import  {pool}  from "../db/db.js";
+import { pool } from "../db/db.js";
 
 class ProductService {
   constructor() {}
@@ -14,6 +14,31 @@ class ProductService {
       });
     });
 
+    return response;
+  }
+
+  getSingleProduct(id) {
+    const response = new Promise((resolve, reject) => {
+      pool.query(
+        `
+      SELECT * ,
+      color.colors_name,
+      brand.brand_name,
+      categories.categories_name
+      FROM products
+      INNER JOIN color ON color.colors_id = products.color_id
+      INNER JOIN brand ON brand.brand_id = products.brand_id
+      INNER JOIN categories ON categories.categories_id = products.categories_id
+      WHERE products.products_id = ${id}`,
+        (error, results) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(results);
+          }
+        }
+      );
+    });
     return response;
   }
 }
